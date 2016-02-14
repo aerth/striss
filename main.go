dd3fb5abc641edc43b057c949f149e0977cc160b
aerth <aerth@isupon.us>
2016-02-13 20:55:33 -0800

    use cli args to get mountpoint

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 	"net"
 	"os"
 	"os/signal"
-	"time"
 	"strconv"
+	"time"
 )
 
 var num_conn uint = 1014
 
 var svr_address string
 var tick_interval uint = 500
+
 const buffer_size = 4 * 1024
 const print_progress = false
 
@@ -31,14 +32,14 @@ func create_getter(uri string,
 		conn, err := net.Dial("tcp", uri)
 		for err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			time.Sleep(5*time.Second)
+			time.Sleep(5 * time.Second)
 			conn, err = net.Dial("tcp", uri)
 		}
 		defer conn.Close()
 
-// This is where you set the mountpoint (until i can figure out the CLI args)
 
-		fmt.Fprint(conn, "GET /stream HTTP/1.0\r\n\r\n")
+		mountpoint := os.Args[2]
+		fmt.Fprint(conn, "GET /" + mountpoint + " HTTP/1.0\r\n\r\n")
 		for {
 			select {
 			case <-quit:
@@ -64,27 +65,25 @@ func create_getter(uri string,
 }
 
 func printUsage() {
-	fmt.Print("\n\nicecast-stress\n\nUsage: ", os.Args[0], " <host.server.com:port> <num_conn> [interval]\n")
-	fmt.Print("Defaults to mountpoint: /stream -- If you need, go in and rebuild!\n\n")
-//	fmt.Print("Example: ", os.Args[0], " example.com:8000 /stream 250\n")
+	fmt.Print("\n\nicecast-stress\n\nUsage: ", os.Args[0], " <host.server.com:port> <mountpoint> <num_conn> [interval] \n")
 }
 
 func init() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 4 {
 		printUsage()
 		os.Exit(-1)
 	}
 
 	svr_address = os.Args[1]
-	if n, e := strconv.ParseUint(os.Args[2], 10, 0); e != nil {
+	if n, e := strconv.ParseUint(os.Args[3], 10, 0); e != nil {
 		printUsage()
 		os.Exit(-1)
 	} else {
 		num_conn = uint(n)
 	}
 
-	if len(os.Args) > 3 {
-		if n, e := strconv.ParseUint(os.Args[3], 10, 0); e != nil {
+	if len(os.Args) > 4 {
+		if n, e := strconv.ParseUint(os.Args[4], 10, 0); e != nil {
 			printUsage()
 			os.Exit(-1)
 		} else {
